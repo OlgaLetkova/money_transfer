80ccd11ed0b0472e3d9fd99f09380f585532e06b
Olga Letkova <olja.let52@gmail.com>
2020-08-27 22:12:21 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int account_balance = 2_000_000_000;
+        int money_transfer = 500_000_000;
+        int total = account_balance + money_transfer;
+        System.out.println(total);}
+}
